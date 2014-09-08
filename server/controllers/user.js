433924dea6fface153e964fd0a36ea7077f59c56
Jakub <jradlak@gmail.com>
2014-09-08 10:20:57 +0200

    finally working uploading files

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,7 +1,8 @@
 var _ =           require('underscore')
     , User =      require('../models/User.js')
     , userRoles = require('../../client/js/routingConfig').userRoles
-    , url = require('url');
+    , url =       require('url')
+    , fs =        require('fs');
 
 module.exports = {
     index: function(req, res) {
@@ -23,15 +24,21 @@ module.exports = {
        User.updateUser(req.db, req.body.username, req.body.role, req.body.description, req.files.avatar, callback)
     },
 
-    uploadAvatar : function (req) {
+    uploadAvatar : function (req, res) {
         console.log('!!! WE ARE IN uploadAvatar');
         // show the uploaded file name
         var fstream;
         req.pipe(req.busboy);
         req.busboy.on('file', function (fieldname, file, filename) {
             console.log("Uploading: " + filename);
+            fstream = fs.createWriteStream(__dirname + '/uploads/' + filename);
+            file.pipe(fstream);
+            fstream.on('close', function () {
+                console.log('FILE SAVED!!!!');
+                res.redirect('back');
+            });
         });
-        
+
         //console.log("file name", req.files.file.name);
         //console.log("file path", req.files.file.path);
     }
